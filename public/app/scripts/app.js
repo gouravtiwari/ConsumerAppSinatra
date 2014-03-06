7d67210f2f7f2d06c8690f42cbc6f68301849c80
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-06 18:32:48 +0530

    I'm loving yo and bo

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
-angular.module('publicApp', []);
+angular.module('publicApp', ['ngRoute'])
+	.config(function ($routeProvider) {
+    $routeProvider
+			.when('/google_maps', {
+			  templateUrl: 'views/google_maps.html',
+			  controller: 'GoogleMapsCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			})
+	});
