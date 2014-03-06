54564b621248599ce9d7f6dd755c16a4980dc6de
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-06 18:58:05 +0530

    Data service included

--- public/app/scripts/controllers/google_maps.js
+++ public/app/scripts/controllers/google_maps.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('GoogleMapsCtrl', function ($scope) {
-	   $scope.map = {
-		    center: {
-		        latitude: 45,
-		        longitude: -73
-		    },
-		    zoom: 8
+  .controller('GoogleMapsCtrl', function ($scope, Data) {
+	  $scope.map = {
+	    center: {
+	        latitude: 45,
+	        longitude: -73
+	    },
+	    zoom: 8
 		};
+
+		Data.get_local('scripts/jsons/data.json').success(function(api_data){
+      console.log(api_data)
+    });
+
   });
--- public/app/scripts/services/data.js
+++ public/app/scripts/services/data.js
@@ -1,6 +1,80 @@
 'use strict';
 
 angular.module('publicApp')
-  .service('Data', function Data() {
+  .service('Data', function Data($http, $rootScope, $timeout, $location, $q, $route) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var Data = {
+
+        cache: {},
+        add_data_to_cache:  function (base_url, api_data) {
+            this.cache[base_url] = api_data;
+            console.log('$$$$$$$$$ CACHE $$$$$$$$$$$');
+            console.log(this.cache);
+        },
+        in_cache: function (base_url) {
+            return this.cache[base_url] || null;
+        },
+        empty_cache: function () {
+            console.log('$$$$$$$$$ CACHE EMPTIED $$$$$$$$$$$');
+            this.cache = {};
+        },
+
+        get_plain_query_url: function (path){
+            return this.settings.base_url+'/api/'+path;
+        },
+
+        get_query_url:  function (path){
+            return this.get_plain_query_url(path)+'?'+this.get_query_params();
+        },
+
+        get_base_json: function(path){
+            var url_calling = this.get_plain_query_url(path);
+            return this.get_promise(url_calling)
+        },
+
+        get_promise: function(url_calling){
+            console.log(url_calling);
+            window.recent_api_url = url_calling
+            var base_url = url_calling.split('?')[0], cached_data, promise;
+            cached_data = this.in_cache(base_url);
+            if(cached_data && base_url != '/api/bkg_progress') {
+                console.log('$$$$$$$$ FOUND IN CACHE $$$$$$$$$');
+                promise = {
+                    success: function(callback){
+                        callback(cached_data);
+                    }
+                }
+            } else {
+                promise = $http.get(url_calling).success(function(api_response){
+                    window.recent_api_response = api_response
+                });   
+                var that = this;
+                promise.success(function(api_data){
+                    if(base_url != '/api/bkg_progress') {
+                        that.add_data_to_cache(base_url, api_data)
+                    }
+                }); 
+            }
+             
+            if(promise.error) {
+                promise.error(function(data, status){
+                    if(status == 303){
+                        console.log('Status 303, starting progress bar...')
+                        $rootScope.job_progress = true;
+                    }
+                });
+            }
+            return promise
+        },
+
+        get_json: function(url_part){
+            return this.get_promise(this.get_query_url(url_part));
+        },
+        
+        get_local: function(path){
+            return this.get_promise(path);
+        }
+
+		}
+    return Data;
   });
