c0fd241b3829e11f1549bb63302b43b4b2e2deb4
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-11 02:31:05 +0530

    Completed MarketShare by Manufacturer API

--- public/app/scripts/controllers/market_share.js
+++ public/app/scripts/controllers/market_share.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('MarketShareCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MarketShareCtrl', function ($scope, Data) {
+    Data.get_local('scripts/jsons/market_share_by_manufacturer.json').success(function(api_data){
+      $scope.periods = api_data.root.PeriodData;
+    });
   });
