36521207748364fae81204bf8e2bc4005a87a47e
Pinakin Mistry <pinakin.nmistry@gmail.com>
2014-03-10 16:15:49 +0530

    Search product by UPC code

--- public/app/scripts/controllers/productReferenceCtrl.js
+++ public/app/scripts/controllers/productReferenceCtrl.js
@@ -3,10 +3,14 @@
 angular.module('publicApp')
   .controller('ProductReferenceCtrl', function ($scope, Data) {
 	  
-	  $scope.input = {Description: 'Search Product...'};
+	  $scope.input = {Description: 'Description...', UPC_CODE: 'UPC Code...'};
 
 		Data.get_local('scripts/jsons/product_by_desc.json').success(function(api_data){
       $scope.products = api_data.ProductDetails;
+    });
+
+    Data.get_local('scripts/jsons/product_by_upc.json').success(function(api_data){
+      $scope.product = api_data.Characteristics[0];
     });
 
   });
