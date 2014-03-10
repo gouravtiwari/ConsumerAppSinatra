02296abb505e9f6626e8144af7183e4342a736a3
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-10 16:50:17 +0530

    Wo hoo look google map shows availability :sunglasses:

--- public/app/scripts/controllers/google_maps.js
+++ public/app/scripts/controllers/google_maps.js
@@ -5,14 +5,25 @@ angular.module('publicApp')
 	  
 	  $scope.map = {
 	    center: {
-	        latitude: 45,
-	        longitude: -73
+	        "latitude":"20",
+          "longitude":"21"
 	    },
-	    zoom: 8
+	    zoom: 12
 		};
 
-		Data.get_local('scripts/jsons/data.json').success(function(api_data){
-      console.log(api_data)
+		Data.get_local('scripts/jsons/product_availability.json').success(function(api_data){
+      $scope.locations = [];
+      $scope.retailers = [];
+      for(var i=0; i<api_data.Product.Availability.length; i++){
+      	for(var j=0; j<api_data.Product.Availability[i].Retailer.length; j++){
+      			$scope.retailers.push(api_data.Product.Availability[i].Retailer[j]);
+      			var position = {latitude: '', longitude: ''};
+      			position.latitude = api_data.Product.Availability[i].Retailer[j].Locations.Latitude;
+      			position.longitude = api_data.Product.Availability[i].Retailer[j].Locations.Longitude;
+      			$scope.locations.push(position);
+      			$scope.map.center = position;
+      	}
+      }
     });
 
   });
