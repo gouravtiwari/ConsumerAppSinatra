8d9a0cb9c11f4495e02e01907d3a99d8bbb66216
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-11 23:32:04 +0530

    playing with market share api json :guitar:

--- public/app/scripts/controllers/market_share.js
+++ public/app/scripts/controllers/market_share.js
@@ -9,8 +9,9 @@ angular.module('publicApp')
           $scope.periods = $scope.periodData;
         };
       };
-    }
+    };
     Data.get_local('scripts/jsons/market_share_by_manufacturer.json').success(function(api_data){
       $scope.periodData = api_data.root.PeriodData;
+      $scope.periods = $scope.periodData;
     });
   });
