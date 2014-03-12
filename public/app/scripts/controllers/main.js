c3c9d4e845681a3ea4caffddb50a066e4484a23e
Pinakin Mistry <pinakin.nmistry@gmail.com>
2014-03-12 09:16:51 +0530

    Finder, Locator,... Business Jargons :sunglasses:

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -8,8 +8,8 @@ angular.module('publicApp')
 
     $scope.navTiles = [
     	{title: 'Top 10 Products', url: '/top_ten'},
-    	{title: 'Search Product', url: '/product_by_desc'},
-    	{title: 'Search <br/> Store', url: '/store_by_name'},
+    	{title: 'Product <br/> Finder', url: '/product_by_desc'},
+    	{title: 'Store <br/> Locator', url: '/store_by_name'},
     	{title: 'Analyse Smartly!', url: '/market_analysis'},
     	{title: 'Compare Smartly!', url: '/compare'},
     	{title: 'Recent <br/> Search', url: '/recent_search'}
