cefe552338dfb41205aad85b65964ec9d2de4cc4
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-13 16:31:51 +0530

    Compare Use case in progress

--- public/app/scripts/controllers/compare.js
+++ public/app/scripts/controllers/compare.js
@@ -3,15 +3,27 @@
 angular.module('publicApp')
   .controller('CompareCtrl', function ($scope, Data) {
     
-  	var parameter_obj = '?numberofresults=5000&sort=desc&';
-
-    Data.get_json('EMM/v1/ios/', parameter_obj).success(function(api_data){
-    	$scope.ios = api_data.AppResponse.AppDetails;
-    	console.log($scope.ios.length)
-    	Data.get_json('EMM/v1/android/', parameter_obj).success(function(api_data){
-    		$scope.android = api_data.AppResponse.AppDetails;
-    		console.log($scope.android.length)
-    	});
+  	var parameter_obj = {'numberofresults' : '100',
+  											 'sort' : 'desc'}
     
+    var sorted_data = [];  											 
+    //Data.get_json('EMM/v1/ios/', parameter_obj).success(function(api_data){
+    Data.get_local('scripts/jsons/ios_apps.json').success(function(api_data){
+    	$scope.ios = api_data.AppResponse.AppDetails;
+    	//Data.get_json('EMM/v1/android/', parameter_obj).success(function(api_data){
+    	Data.get_local('scripts/jsons/android_apps.json').success(function(api_data){
+    		$scope.android = api_data.AppResponse.AppDetails;
+    		for(var i=0; i<$scope.ios.length; i++){
+    			for(var j=0; j<$scope.android.length; j++){
+    				if($scope.ios[i].AppName == $scope.android[j].AppName){
+    					var obj = {};
+    					obj[$scope.ios[i].AppName] = {android: $scope.ios[i], ios: $scope.android[j]};
+    					sorted_data.push(obj)
+    				}
+    			}
+    		}
+    		console.log(sorted_data)
+    	});
     });
+
   });
