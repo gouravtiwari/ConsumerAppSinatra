2a6550ae9ecbc495868f3dd2889e525902cd8ae6
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-14 10:39:54 +0530

    filter directive added

---
+++ public/app/scripts/directives/stringfilter.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('publicApp')
+  .directive('stringfilter', function () {
+    return {
+      template: '<div>{{dataval}}</div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        scope.dataval = attrs.dataval
+        if(attrs.filter){
+        	scope.dataval = attrs.dataval.split(attrs.filter)[1]
+        }
+        if(attrs.filtertype == 'onlystring'){
+        	scope.dataval = scope.dataval.replace('/_/g', ' ');
+        }
+        else{
+        	scope.dataval = scope.dataval.replace('_', ' - ');
+        }
+      }
+    };
+  });
