1596d91819139bf11440fd19510667866486d58a
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-14 16:32:23 +0530

    Product reference all apis's integrated

--- public/app/scripts/controllers/productReferenceCtrl.js
+++ public/app/scripts/controllers/productReferenceCtrl.js
@@ -4,14 +4,7 @@ angular.module('publicApp')
   .controller('ProductReferenceCtrl', function ($scope, Data) {
 	  
 	  $scope.input = {Description: 'Description...', UPC_CODE: 'UPC Code...'};
-
-    Data.get_local('scripts/jsons/product_by_upc.json').success(function(api_data){
-      $scope.product = api_data.Characteristics[0];
-    });
-
-    Data.get_local('scripts/jsons/product_health.json').success(function(api_data){
-      $scope.product = api_data.Characteristics[0];
-    });
+    $scope.pageshow = false;
 
     $scope.currentPage = 1;
 
@@ -20,9 +13,23 @@ angular.module('publicApp')
                           'pageno' : $scope.currentPage};
       Data.get_json('Products/v1', parameter_obj).success(function(api_data){
       //Data.get_local('scripts/jsons/product_by_desc.json').success(function(api_data){
+        $scope.pageshow = true;
         $scope.products = api_data.ProductDetails;
         $scope.totalItems = api_data.Summary.TotalPages;
         $scope.maxSize = 10;
+      });
+    }
+
+    $scope.by_upc = function(){
+      var parameter_obj = {'search' : $scope.input.UPC_CODE};
+
+      Data.get_json('Products/v1', parameter_obj).success(function(api_data){
+      //Data.get_local('scripts/jsons/product_by_desc.json').success(function(api_data){
+        $scope.product_detail = api_data.ProductDetails;
+        Data.get_json('Products/v1/'+ $scope.input.UPC_CODE, '').success(function(api_data){
+        //Data.get_local('scripts/jsons/product_by_desc.json').success(function(api_data){
+        $scope.product_detail_characterstics = api_data.Characteristics;
+        });
       });
     }
 
