3cb534de0b72a9ddb3dde068fec936c0e5fd3d5c
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-14 09:10:57 +0530

    Recommender boiler-plate

---
+++ public/app/scripts/controllers/recommender.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('publicApp')
+  .controller('RecommenderCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
