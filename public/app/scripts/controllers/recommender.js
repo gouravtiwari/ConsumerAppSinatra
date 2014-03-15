9c77fb7d75cb037a18decc914976d24f0d0b2a6c
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-15 19:16:18 +0530

    Categories are dynamic now one more step closer :sunglasses:

--- public/app/scripts/controllers/recommender.js
+++ public/app/scripts/controllers/recommender.js
@@ -3,19 +3,43 @@
 angular.module('publicApp')
   .controller('RecommenderCtrl', function ($scope, Data) {
     $scope.categories = ['Television', 'Mobile Apps'];
-    $scope.selectedSubCategory1 = {"obj":{}};
-    $scope.selectedDemography1 = {"obj":{}};
-    $scope.selectedItem1 = {"obj":{}};
-    
-    $scope.selectedSubCategory2 = {"obj":{}};
-    $scope.selectedDemography2 = {"obj":{}};
-    $scope.selectedItem2 = {"obj":{}};
-    
-    $scope.selectedSubCategory3 = {"obj":{}};
-    $scope.selectedDemography3 = {"obj":{}};
-    $scope.selectedItem3 = {"obj":{}};
+    $scope.numberOfRows = [1,2,3,4,5,6,7,8,9,10];
 
+    $scope.selectedSubCategory1 = {"obj":{}};
+    $scope.selectedSubCategory2 = {"obj":{}};
+    $scope.selectedSubCategory3 = {"obj":{}};
+    $scope.selectedSubCategory4 = {"obj":{}};
+    $scope.selectedSubCategory5 = {"obj":{}};
+    $scope.selectedSubCategory6 = {"obj":{}};
+    $scope.selectedSubCategory7 = {"obj":{}};
+    $scope.selectedSubCategory8 = {"obj":{}};
+    $scope.selectedSubCategory9 = {"obj":{}};
+    $scope.selectedSubCategory10 = {"obj":{}};
+
+    $scope.selectedDemography1 = {"obj":{}};
+    $scope.selectedDemography2 = {"obj":{}};
+    $scope.selectedDemography3 = {"obj":{}};
+    $scope.selectedDemography4 = {"obj":{}};
+    $scope.selectedDemography5 = {"obj":{}};
+    $scope.selectedDemography6 = {"obj":{}};
+    $scope.selectedDemography7 = {"obj":{}};
+    $scope.selectedDemography8 = {"obj":{}};
+    $scope.selectedDemography9 = {"obj":{}};
+    $scope.selectedDemography10 = {"obj":{}};
+
+    $scope.selectedItem1 = {"obj":{}};
+    $scope.selectedItem2 = {"obj":{}};
+    $scope.selectedItem3 = {"obj":{}};
+    $scope.selectedItem4 = {"obj":{}};
+    $scope.selectedItem5 = {"obj":{}};
+    $scope.selectedItem6 = {"obj":{}};
+    $scope.selectedItem7 = {"obj":{}};
+    $scope.selectedItem8 = {"obj":{}};
+    $scope.selectedItem9 = {"obj":{}};
+    $scope.selectedItem10 = {"obj":{}};
+    
     $scope.selectedItemsList = [];
+
     $scope.$watch('categoryName1', function() {
       $scope.searchByCategory();
     });
@@ -23,6 +47,27 @@ angular.module('publicApp')
       $scope.searchByCategory();
     });
     $scope.$watch('categoryName3', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName4', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName5', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName6', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName7', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName8', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName9', function() {
+      $scope.searchByCategory();
+    });
+    $scope.$watch('categoryName10', function() {
       $scope.searchByCategory();
     });
 
@@ -47,9 +92,52 @@ angular.module('publicApp')
           $scope.selectedCategories3 = api_data.Category;
         });
       }
+      if ($scope.categoryName4){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories4 = api_data.Category;
+        });
+      }
+      if ($scope.categoryName5){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories5 = api_data.Category;
+        });
+      }
+      if ($scope.categoryName6){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories6 = api_data.Category;
+        });
+      }
+      if ($scope.categoryName7){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories7 = api_data.Category;
+        });
+      }
+      if ($scope.categoryName8){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories8 = api_data.Category;
+        });
+      }
+      if ($scope.categoryName9){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories9 = api_data.Category;
+        });
+      }
+      if ($scope.categoryName10){
+        // Data.get_json('TopTen/v1/' + $scope.categoryName, parameter_obj).success(function(api_data){
+        Data.get_local('scripts/jsons/top_ten_mobile_apps.json').success(function(api_data){
+          $scope.selectedCategories10 = api_data.Category;
+        });
+      }
     };
 
     $scope.recommender = function(){
+
       $scope.selectedItemsList = [{
           "category": $scope.categoryName1,
           "subCategory": $scope.selectedSubCategory1.obj.SubCategoryName,
@@ -65,8 +153,44 @@ angular.module('publicApp')
           "subCategory": $scope.selectedSubCategory3.obj.SubCategoryName,
           "demography": $scope.selectedDemography3.obj.DemographyName,
           "item": $scope.selectedItem3.obj
+        },{
+          "category": $scope.categoryName4,
+          "subCategory": $scope.selectedSubCategory4.obj.SubCategoryName,
+          "demography": $scope.selectedDemography4.obj.DemographyName,
+          "item": $scope.selectedItem4.obj
+        },{
+          "category": $scope.categoryName5,
+          "subCategory": $scope.selectedSubCategory5.obj.SubCategoryName,
+          "demography": $scope.selectedDemography5.obj.DemographyName,
+          "item": $scope.selectedItem5.obj
+        },{
+          "category": $scope.categoryName6,
+          "subCategory": $scope.selectedSubCategory6.obj.SubCategoryName,
+          "demography": $scope.selectedDemography6.obj.DemographyName,
+          "item": $scope.selectedItem6.obj
+        },{
+          "category": $scope.categoryName7,
+          "subCategory": $scope.selectedSubCategory7.obj.SubCategoryName,
+          "demography": $scope.selectedDemography7.obj.DemographyName,
+          "item": $scope.selectedItem7.obj
+        },{
+          "category": $scope.categoryName8,
+          "subCategory": $scope.selectedSubCategory8.obj.SubCategoryName,
+          "demography": $scope.selectedDemography8.obj.DemographyName,
+          "item": $scope.selectedItem8.obj
+        },{
+          "category": $scope.categoryName9,
+          "subCategory": $scope.selectedSubCategory9.obj.SubCategoryName,
+          "demography": $scope.selectedDemography9.obj.DemographyName,
+          "item": $scope.selectedItem9.obj
+        },{
+          "category": $scope.categoryName10,
+          "subCategory": $scope.selectedSubCategory10.obj.SubCategoryName,
+          "demography": $scope.selectedDemography10.obj.DemographyName,
+          "item": $scope.selectedItem10.obj
         }
       ];
+      $scope.selectedItemsList.splice($scope.rowsToGenerate, 10 - $scope.rowsToGenerate);
       $scope.recommendationScore = $scope.computeScore();
       $scope.recommendationRating = $scope.computeRating($scope.recommendationScore);
     };
