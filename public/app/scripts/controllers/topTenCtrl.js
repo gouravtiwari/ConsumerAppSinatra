d6cb562592a15ad3ca4b5bc177bb998fa86f0d4d
Aditya Sethi <aditya_sethi@yahoo.co.in>
2014-03-16 01:13:56 +0530

    Loved this generic integration for top 10 api :<3: :sunglasses: :guitar:

--- public/app/scripts/controllers/topTenCtrl.js
+++ public/app/scripts/controllers/topTenCtrl.js
@@ -2,21 +2,45 @@
 
 angular.module('publicApp')
   .controller('TopTenCtrl', function ($scope, Data) {
-    $scope.categories = ['Movies', 'Television', 'Books', 'Mobile Apps', 'Video Games', 'Websites', 'Products', 'Music and Twitter TV'];
-    $scope.$watch('categoryName', function() {
-      if ($scope.resultCategories != undefined){
-        for (var i = 0; i<$scope.resultCategories.length; i++) {
-          if ($scope.resultCategories[i].CategoryName == $scope.categoryName){
-            $scope.SubCategory = $scope.resultCategories[i].SubCategory;
+    
+    $scope.categories = ['Movies', 'Television', 'Books', 'Mobile Apps', 'Video Games', 'Websites', 'Products', 'Music', 'Twitter TV'];
+    $scope.categoryName = $scope.categories[0];
+    categories_loader();
+    
+    function categories_loader(){
+      Data.get_json('TopTen/v1/'+$scope.categoryName).success(function(api_data){
+        $scope.subCategories =  api_data.Category[0].SubCategory;
+        $scope.subCategoryName = $scope.subCategories[0];
+        $scope.demographies = $scope.subCategories[0].Demographies;
+        $scope.demography = $scope.demographies[0]
+        data_loader();
+      });  
+    }
+
+    function data_loader(){
+      for (var i=0; i<$scope.subCategories.length; i++){
+        if($scope.subCategories[i].SubCategoryName == $scope.subCategoryName.SubCategoryName){
+          for (var j = 0; j < $scope.subCategories[i].Demographies.length; j++) {
+            if ($scope.subCategories[i].Demographies[j].DemographyName == $scope.demography.DemographyName){
+              $scope.items = $scope.subCategories[i].Demographies[j].Items;
+              console.log($scope.items);
+            }
           }
-          else{
-            $scope.SubCategory = '';
-          }
-        };
+        }
       }
-     });
-    Data.get_local('scripts/jsons/top_ten.json').success(function(api_data){
-      $scope.resultCategories = api_data.Category;
-      $scope.categoryName = $scope.categories[0];
+    }
+    
+    $scope.$watch('categoryName', function() {
+      categories_loader();
+    });
+
+    $scope.$watch('demography', function() {
+      console.log('hi')
+      data_loader();
+    });
+  
+    $scope.$watch('subCategoryName', function() {
+      console.log('hi')
+      data_loader();
     });
   });
