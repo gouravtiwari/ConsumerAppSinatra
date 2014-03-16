cb276c0c049d450c58bea3e11529f93d27da3ea7
Aditya Sethi <aditya_sethi@yahoo.co.in>
2014-03-16 20:50:27 +0530

    Filters for M B and K

--- public/app/scripts/filters/roundoff.js
+++ public/app/scripts/filters/roundoff.js
@@ -5,4 +5,23 @@ angular.module('publicApp')
     return function (input) {
       return parseFloat(input).toFixed(2);
     };
+  })
+
+  .filter('unitCon', function () {
+    return function (input) {
+      var data = parseFloat(input);
+      if(data < 1000){
+      	data = data;
+      }
+      else if(data>= 1000 && data < 1000000){
+      	data = parseFloat(data/1000).toFixed(2) + 'K';
+      }
+      else if(data>=1000000 && data<10000000000){
+      	data = parseFloat(data/1000000).toFixed(2) + 'M';
+      }
+      else{
+      	data = parseFloat(data/1000000000).toFixed(2) + 'B';
+      }
+      return data;
+    };
   });
