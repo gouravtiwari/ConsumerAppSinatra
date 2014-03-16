e86a1ae9c8c8b9e98a3b44d0c55f05f51920c5d7
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-16 17:04:07 +0530

    fixed final alignment and some styling for recommender

--- public/app/scripts/directives/doughnut.js
+++ public/app/scripts/directives/doughnut.js
@@ -2,13 +2,8 @@
 
 angular.module('publicApp')
 .directive('doughnut', function () {
-  // var margin = 20,
-  //   width = 960,
-  //   height = 500 - .5 - margin,
-  //   color = d3.interpolateRgb("#f77", "#77f");
   return {
     restrict: 'E',
-    // template: '<div class="donut"></div>',
     scope: {
         chartData: "=chartId",
         val: '=',
@@ -22,16 +17,13 @@ angular.module('publicApp')
       console.log("selector:"+ selector);
         //Update when charts data changes
         scope.$watch('val', function () {
-          // console.log(newVal);
-          // console.log(oldVal);
           // clear the elements inside of the directive
-          // selector.selectAll('*').remove();
+          $("doughnut").html('');
 
           // if 'val' is undefined, exit
           if (!scope.val) {
             return;
           }
-          console.log('val: '+ scope.val);
           donutTip({
                     selector: selector, 
                     data: scope.val,
