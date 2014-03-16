a63ae3bd3d19e4a3da54ee811de25951bdf836e7
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-16 18:10:10 +0530

    API integration done for TV Program Rankings

--- public/app/scripts/controllers/program_rankings.js
+++ public/app/scripts/controllers/program_rankings.js
@@ -2,7 +2,17 @@
 
 angular.module('publicApp')
   .controller('ProgramRankingsCtrl', function ($scope, Data) {
-    Data.get_local('scripts/jsons/program_rankings.json').success(function(api_data){
-      $scope.programs = api_data.NationalTVRanking.Programs;
-    });
+    $scope.search = function(){
+      var parameter_obj = {
+                            "noofresults": $scope.input.numberofresults,
+                            "demographies": $scope.input.demography
+                          };
+      // demographies=M2-11&originator=TURNER NEEDs to be added
+      Data.get_json('NationalTV/v1/', parameter_obj).success(function(api_data){
+      // Data.get_local('scripts/jsons/program_rankings.json').success(function(api_data){
+        $scope.programs = api_data.NationalTVRanking.Programs;
+      });
+  
+    };
+    
   });
