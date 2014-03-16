0ebab15d4a1ec6f1a531272becac66e7d5b3f1fc
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-17 02:44:03 +0530

    TV Program now has API integrated with doughnut chart

--- public/app/scripts/controllers/program_rankings.js
+++ public/app/scripts/controllers/program_rankings.js
@@ -3,17 +3,33 @@
 angular.module('publicApp')
   .controller('ProgramRankingsCtrl', function ($scope, Data) {
     $scope.demographies = ['M2-11'];
+    $scope.$watch('programs', function() {
+      $scope.doughnutRedraw();
+    });
     $scope.search = function(){
       var parameter_obj = {
                             "noofresults": $scope.input.numberofresults,
                             "demographies": $scope.input.demography
                           };
       // demographies=M2-11&originator=TURNER NEEDs to be added
-      Data.get_json('NationalTV/v1/', parameter_obj).success(function(api_data){
-      // Data.get_local('scripts/jsons/program_rankings.json').success(function(api_data){
+      // Data.get_json('NationalTV/v1/', parameter_obj).success(function(api_data){
+      Data.get_local('scripts/jsons/program_rankings.json').success(function(api_data){
         $scope.programs = api_data.NationalTVRanking.Programs;
       });
-  
     };
+
+    $scope.doughnutRedraw = function(){
+      if($scope.programs != undefined && $scope.programs != null && $scope.programs.toString() != 'NaN'){
+        $scope.data = [];
+        for (var i = 0; i < $scope.programs.length; i++) {
+          $scope.data[i] = {'label': '#'+i+'. '+$scope.programs[i].ProgramName +
+                                      ", Originator: " + $scope.programs[i].Originator +
+                                      ", AA %: " + $scope.programs[i].US_AA_PERC,
+
+                            'value': $scope.programs[i].USProjection};
+        };
+      }
+    };
+
     
   });
