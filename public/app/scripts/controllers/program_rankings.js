47804804e6273441032e28e0135498b712f2f3ee
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-19 06:31:40 +0530

    added a heat map and chart to national TV program thanks @pinakinmistry

--- public/app/scripts/controllers/program_rankings.js
+++ public/app/scripts/controllers/program_rankings.js
@@ -2,6 +2,7 @@
 
 angular.module('publicApp')
   .controller('ProgramRankingsCtrl', function ($scope, Data) {
+    $scope.sortByFields = [];
     $scope.demographies = ['M2-11'];
     $scope.$watch('programs', function() {
       $scope.doughnutRedraw();
@@ -14,9 +15,50 @@ angular.module('publicApp')
       // demographies=M2-11&originator=TURNER NEEDs to be added
       // Data.get_json('NationalTV/v1/', parameter_obj).success(function(api_data){
       Data.get_local('scripts/jsons/program_rankings.json').success(function(api_data){
-        $scope.programs = api_data.NationalTVRanking.Programs;
+        if(!api_data.NationalTVRanking.Programs) {
+          $scope.output.message = "No Record found for the provided input";
+          $scope.programs = [];
+        } else {
+          $scope.output.message = '';
+          $scope.programs = api_data.NationalTVRanking.Programs;
+          $scope.sortByFields = Data.fillSortByFields($scope.programs[0]);
+          Data.injectColorClass($scope.programs, $scope.sortByFields);  
+        }
       });
     };
+
+    if($scope.viaRecentSearch) {
+      if($scope.cache_response.NationalTVRanking && !$scope.cache_response.NationalTVRanking.Programs) {
+        $scope.output.message = "No Record found for the provided input";
+        $scope.netUsageData = '';
+      } else {
+        $scope.programs = $scope.cache_response.NationalTVRanking.Programs;
+        $scope.sortByFields = Data.fillSortByFields($scope.programs[0]);
+        Data.injectColorClass($scope.programs, $scope.sortByFields);
+        $scope.output.message = '';
+
+      }
+      $scope.viaRecentSearch = false;
+    }
+
+    $scope.$watch('cache_response', function(newValue, oldValue){
+      if(newValue == oldValue) { return; }
+      if($scope.cache_response.NationalTVRanking) {
+        if(!$scope.cache_response.NationalTVRanking.Programs) {
+          $scope.output.message = "No Record found for the provided input";
+          $scope.netUsageData = '';
+        } else {
+          $scope.netUsageData = $scope.cache_response.NationalTVRanking.Programs;
+          $scope.output.message = '';
+        }
+      }
+    });
+
+    $scope.$watch('sortBy', function(newvalue, oldvalue){
+      console.log(newvalue)
+      if(!newvalue || newvalue == oldvalue) return;
+      $scope.programs = Data.sortBy(newvalue, $scope.programs);
+    })
 
     $scope.doughnutRedraw = function(){
       $scope.totalUSProjection = 0;
@@ -27,16 +69,19 @@ angular.module('publicApp')
                         'outputColor': '#000000',
                         'outputValueColor': '#45C0B6',
                         'tipLabel': 'Projected Audience',
-                        'tipLabelUnit': ''
+                        'tipLabelUnit': '',
+                        'legend': true,
+                        'width': 350,
+                        'height': 350,
+                        'innerRadius': 0,
+                        'outerRadius': 120,
+                        'animate': false
                       };
         for (var i = 0; i < $scope.programs.length; i++) {
           $scope.data.list[i] = {'label': '#'+(i+1)+'. '+$scope.programs[i].ProgramName + '<br>' + 
                                       "Originator: " + $scope.programs[i].Originator + '<br>' +
                                       "US AA %: " + $scope.programs[i].US_AA_PERC,
                             'value': parseInt($scope.programs[i].USProjection)};
-          console.log(parseInt($scope.programs[i].USProjection));
-          console.log(i);
-          console.log($scope.totalUSProjection);
           $scope.totalUSProjection = $scope.totalUSProjection + parseInt($scope.programs[i].USProjection);
         };
       }
